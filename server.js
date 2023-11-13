beb2b14c8abfa46e2f540cf7a4d5dac0ef84f0ee
Borna Stankovic <stankovicborna@gmail.com>
2023-11-13 04:52:39 +0100

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const { Pool } = require('pg');
 require("dotenv").config();
 
 const app = express();
